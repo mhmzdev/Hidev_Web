799ca1a788321e6d884f04ba72a7fa6732e98012
m-hamzashakeel <hamza.6.shakeel.com>
2020-07-15 06:12:17 +0500

    Front end completed

--- script.js
+++ script.js
@@ -14,3 +14,9 @@ $('textarea').each(function () {
   this.style.height = 'auto';
   this.style.height = (this.scrollHeight) + 'px';
 });
+
+
+$('.navbar-nav .nav-link').click(function(){
+  $('.navbar-nav .nav-link').removeClass('active');
+  $(this).addClass('active');
+})
