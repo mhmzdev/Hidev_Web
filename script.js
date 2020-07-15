8cef0bb350487427d151b0cf08af0c7947601c6e
m-hamzashakeel <hamza.6.shakeel.com>
2020-07-15 18:04:26 +0500

    Animations added

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ $(document).ready(function () {
       scrollTop: 0
     }, 700);
   });
+
+  AOS.init({
+    easing: "ease",
+    duration: 1200,
+    once: true
+  });
 });
 
 
@@ -16,7 +22,38 @@ $('textarea').each(function () {
 });
 
 
-$('.navbar-nav .nav-link').click(function(){
+$('.navbar-nav .nav-link').click(function () {
   $('.navbar-nav .nav-link').removeClass('active');
   $(this).addClass('active');
 })
+
+
+$(window).scroll(statsIncrement);
+var viewed = false;
+
+function isScrolledIntoView(elem) {
+  var docViewTop = $(window).scrollTop();
+  var docViewBottom = docViewTop + $(window).height();
+
+  var elemTop = $(elem).offset().top;
+  var elemBottom = elemTop + $(elem).height();
+
+  return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+function statsIncrement() {
+  if (isScrolledIntoView($(".statsContainer")) && !viewed) {
+    viewed = true;
+    $('.value').each(function () {
+      $(this).prop('Counter', 0).animate({
+        Counter: $(this).text()
+      }, {
+        duration: 500,
+        easing: 'swing',
+        step: function (now) {
+          $(this).text(Math.ceil(now));
+        }
+      });
+    });
+  }
+}
